f7a213e79c42ab0c86aa79982acc3fc64955f1ab
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-17 00:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysInYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        // Leap year has 366 days
+        return 366;
+    } else {
+        // Non-leap year has 365 days
+        return 365;
+    }
+       
+}
+
+let input= parseInt(prompt("type year"));
+alert(daysInYear(input)) ;
