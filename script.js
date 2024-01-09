aa98b93b62e7648a770973a2d489e8d5c35e424b
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2024-01-09 13:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
-function daysInYear(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+function daysInYear(year) { 
+    if ((year % 400 === 0 &&year % 4==0)){ 
+		
         // Leap year has 366 days
-        return 366;
+        return 366;    
     } else {
         // Non-leap year has 365 days
         return 365;
